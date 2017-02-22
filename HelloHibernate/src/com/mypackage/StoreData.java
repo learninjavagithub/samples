8b5a7f239343cb01a19ef3495a44baefdcdffe75
Praveen <praveenkb146@gmail.com>
2017-02-22 19:26:00 +0530

    Added HBM sample

---
+++ HelloHibernate/src/com/mypackage/StoreData.java
@@ -0,0 +1,49 @@
+package com.mypackage;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class StoreData {
+	
+	public static void main(String[] args) {
+
+		// creating configuration object
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");// populates the data of the
+											// configuration file
+
+		// creating seession factory object
+		SessionFactory factory = cfg.buildSessionFactory();
+
+		// creating session object
+		Session session = factory.openSession();
+
+		// creating transaction object
+		Transaction t = session.beginTransaction();
+
+		Employee e1 = new Employee();
+		e1.setId(115);
+		e1.setFirstName("Ganesh");
+		e1.setLastName("Venkatesh");
+
+		//session.persist(e1);// persisting the object - Called during first insert
+		session.update(e1);
+
+		Employee e2 = (Employee) session.load(Employee.class, 117);
+		//System.out.println("Loaded Employee from DB with Id : " + e2.getLastName()); // Throws ObjectNotFoundException
+		System.out.println("Loaded Employee from DB with Id : " + e2.getId()); // Works fine as only Id is accessed and displays 117
+
+		Employee e3 = (Employee) session.get(Employee.class, 115); // Throws ObjectNotFoundException here itself if id not found
+		System.out.println("Get Employee from DB with Id : " + e3.getId()); // Displays 115
+		
+		
+		t.commit();// transaction is commited
+		session.close();
+		factory.close();// If this statement is not present, then the program will be running for ever
+
+		System.out.println("successfully saved");
+
+	}
+}
